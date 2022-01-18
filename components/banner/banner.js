ac492ff75eec62f3685b6b921a2c2de053d6d092
St3ve89 <istvan.acs89@gmail.com>
2022-01-18 13:56:36 +0000

    Start adding styles to the banner component

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,20 +1,30 @@
+import styles from './banner.module.css';
+
 const Banner = ({ title, subTitle, imgUrl }) => {
   const handleOnPlay = () => {
     console.log('handle on play');
   };
   return (
-    <div>
-      <h3>{title}</h3>
-      <h3>{subTitle}</h3>
-      <button onClick={handleOnPlay}>Play</button>
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
       <div
+        className={styles.bannerImg}
         style={{
-          backgroundImage: `url(${imgUrl})`,
-          width: '100%',
-          height: '100%',
-          position: 'absolute',
-          backgroundSize: 'cover',
-          backgroundPosition: '50% 50%',
+          backgroundImage: `linear-gradient(to top right, rgba(228, 221, 221, 0.2) 10%, rgba(0,0,0,0.3) 95%, url(${imgUrl})`,
         }}
       ></div>
     </div>
